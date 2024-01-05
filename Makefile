bfef65be81aa38fe44c8b98be5f3008ae278ebe7
Jake Lazaroff <yo@jake.nyc>
2024-01-05 00:39:49 -0500

    Finish open graph images

--- Makefile
+++ Makefile
@@ -1,10 +1,18 @@
+.DEFAULT_GOAL = help
+
+##@ Development
 .PHONY: dev build link
 
-dev: link
+dev: link  ## Run the development webserver
 	@bun dev
 
-build: link
+build: link  ## Build for production
 	@bun build
 
-link:
+link: ## Create symlinks for the `til` submodule in `src/content/til`
 	@for dir in $(dir $(wildcard til/*/.)); do ln -sf "../../../$$dir" "./src/content/til"; done
+
+##@ Help
+
+help:  ## Display this help
+	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[.a-zA-Z_-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
--- src/pages/og/[...slug].png.ts
+++ src/pages/og/[...slug].png.ts
@@ -1,10 +1,9 @@
-import { getCollection } from "astro:content";
+import { getCollection, getEntryBySlug, type CollectionEntry } from "astro:content";
 
 export async function getStaticPaths() {
   const entries = await getCollection("til");
   return entries.map(post => ({
-    params: { slug: post.slug },
-    props: post
+    params: { slug: post.slug }
   }));
 }
 
@@ -12,34 +11,67 @@ import fs from "node:fs/promises";
 import satori from "satori";
 import sharp from "sharp";
 import type { APIRoute } from "astro";
+import { getInfo } from "../../util/entry";
 
-export const GET: APIRoute = async function ({ params, request }) {
+export const GET: APIRoute = async function GET({ params }) {
   const overpass = await fs.readFile("./src/style/overpass-regular.ttf");
+  const overpassBold = await fs.readFile("./src/style/overpass-bold.ttf");
+  const jetbrainsmono = await fs.readFile("./src/style/jetbrainsmono-italic.ttf");
+
+  const entry = await getEntryBySlug("til", params.slug as CollectionEntry<"til">["slug"]);
+  const info = await getInfo(entry);
 
   const svg = await satori(
     {
-      type: "h1",
-      props: { children: "Hello world" }
+      type: "div",
+      props: {
+        style: {
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "#fff",
+          padding: "5%",
+          fontFamily: "Overpass",
+          letterSpacing: "-1ch",
+          lineHeight: 1,
+          textWrap: "balanced"
+        },
+        children: [
+          { type: "div", props: { style: { fontSize: 32 }, children: "Today I Learned" } },
+          {
+            type: "div",
+            props: {
+              style: { display: "flex", flexDirection: "column", marginTop: "auto" },
+              children: [
+                {
+                  type: "p",
+                  props: {
+                    style: { fontSize: 32, fontFamily: "JetBrains Mono", color: "#00000066" },
+                    children: info.category
+                  }
+                },
+                {
+                  type: "div",
+                  props: { style: { fontSize: 64, fontWeight: 700 }, children: info.title }
+                }
+              ]
+            }
+          }
+        ]
+      }
     },
     {
       width: 1200,
       height: 630,
       fonts: [
-        {
-          name: "Overpass",
-          data: overpass,
-          weight: 400,
-          style: "normal"
-        }
+        { name: "Overpass", data: overpass, weight: 400, style: "normal" },
+        { name: "Overpass", data: overpassBold, weight: 700, style: "normal" },
+        { name: "JetBrains Mono", data: jetbrainsmono, weight: 400, style: "italic" }
       ]
     }
   );
 
   const png = await sharp(Buffer.from(svg)).png().toBuffer();
-
-  return new Response(png, {
-    headers: {
-      "Content-Type": "image/png"
-    }
-  });
+  return new Response(png, { headers: { "Content-Type": "image/png" } });
 };
