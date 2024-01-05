1143628e5b236e977ce6ff2b9fa81d21a12b1850
Jake Lazaroff <yo@jake.nyc>
2024-01-05 13:23:54 -0500

    Add pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://til.jakelazaroff.com",
+  integrations: [pagefind()],
   markdown: { shikiConfig: { theme: "material-theme-palenight" } },
   vite: { resolve: { preserveSymlinks: true } }
 });
